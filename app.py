a5cbf1a932ddc3c9a64781c25bc0ef2ff07bf9fc
Patrick Regis <patsrgsreis@gmail.com>
2026-03-13 15:02:34 +0000

    app + main
    fix 1
    
    requirements + app.py fix
    
    better code
    
    fix: dropdown nao atualizava com voto
    
    fix 3
    
    fix 4
    
    temp
    
    tentando consertar tela branca pos voto
    
    tentando sem markdown
    
    fix2
    
    state da classe selecionada + fix tela branca
    
    fuck
    
    fix: visuais + multi voto
    
    lógica de telas
    
    i dontk know anymore
    
    dd

---
+++ app.py
@@ -0,0 +1,96 @@
+import streamlit as st
+import pandas as pd
+from streamlit_gsheets import GSheetsConnection
+from datetime import datetime
+import time
+import pytz
+
+st.set_page_config(page_title="Urna Eletrônica", layout="wide")
+st.title("🗳️ Eleição do Grêmio Estudantil")
+
+# 1. GSheets
+conn = st.connection("gsheets", type=GSheetsConnection)
+
+students_df = conn.read(worksheet="alunos", ttl=0)
+candidates_df = conn.read(worksheet="candidatos", ttl=0)
+
+students_df['ja_votou'] = pd.to_numeric(students_df['ja_votou'], errors='coerce').fillna(0).astype(int)
+
+# Variáveis de Estado
+
+if "current_class" not in st.session_state:
+    st.session_state.current_class = students_df["turma"].dropna().unique()[0]
+
+# Sidebar - Área do Mesário
+
+st.sidebar.header("Área do Mesário")
+available_classes = students_df["turma"].dropna().unique()
+
+selected_class = st.sidebar.selectbox(
+    "1. Selecione a Turma:", 
+    available_classes,
+    index=list(available_classes).index(st.session_state.current_class),
+    key="class_selector"
+)
+st.session_state.current_class = selected_class
+
+# Filtro (turma + não votou)
+
+class_students = students_df[(students_df["turma"] == selected_class) & (students_df["ja_votou"] == 0)]
+
+student_options = ["-- Selecione o Estudante --"] + class_students["nome"].tolist()
+selected_student = st.sidebar.selectbox(
+    "2. Selecione o Estudante:", 
+    student_options,
+    key=f"select_{selected_class}"
+)
+
+# Trava lógica pós voto
+
+can_vote = False
+
+if selected_student != "-- Selecione o Estudante --":
+    st.info(f"Urna liberada para: {selected_student}")
+    can_vote = True  # Destrava
+else:
+    st.info("Aguardando mesário selecionar um aluno...")
+    can_vote = False # Trava
+
+st.divider()
+
+st.subheader("Toque no seu candidato:")
+candidates_list = candidates_df["candidato"].dropna().tolist()
+num_cols = 2
+
+for i in range(0, len(candidates_list), num_cols):
+    batch = candidates_list[i : i + num_cols]
+    cols = st.columns(num_cols)
+
+    for j, name in enumerate(batch):
+        actual_index = i + j
+        with cols[j]:
+            img = candidates_df.iloc[actual_index]['link_img']
+            if pd.notna(img) and img != "x":
+                st.image(img, use_container_width=True)
+
+            if st.button(f"VOTAR: {name}", key=f"btn_{actual_index}", use_container_width=True, disabled = not can_vote):
+                    
+                with st.spinner("Gravando..."):
+                        students_df.loc[students_df['nome'] == selected_student, 'ja_votou'] = 1
+                        conn.update(worksheet="alunos", data=students_df)
+                        
+                        # Atualiza Votos
+                        fuse_br = pytz.timezone("America/Sao_Paulo")
+                        now_br = datetime.now(fuse_br)
+                        votes_df = conn.read(worksheet="votos", ttl=0)
+                        new_vote = pd.DataFrame([{
+                            "data_hora": now_br.strftime("%d/%m/%Y %H:%M:%S"),
+                            "candidato_votado": name
+                        }])
+                        updated_votes_df = pd.concat([votes_df, new_vote], ignore_index=True)
+                        conn.update(worksheet="votos", data=updated_votes_df)
+                    
+                    # Dispara a tela de FIM
+                    st.session_state.last_voter = selected_student
+                    st.session_state.show_fim = True
+                    st.rerun()
